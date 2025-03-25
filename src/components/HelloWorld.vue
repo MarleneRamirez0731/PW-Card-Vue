bca0d48e2206ea09d886c95ecc4945f96a383392
Marlene Ramírez0731 <marlenmaricela09@gmail.com>
2025-03-24 22:16:00 -0600

    Pagina web

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,87 @@
+<script setup>
+//metodos 
+</script>
+
+<template>
+  <!-- estructura HTML -->
+   <div class="container">
+    <h1 class="text-center">NATURALEZA</h1>
+    <!-- INICIO DEL PRIMER ROW -->
+    <div class="row">
+      <div class="col-md-6">
+        <div class="card">
+          <div style="background-color: #85fa68;" class="card-header">
+            <h6>Flores</h6>
+
+          </div>
+          <div  class="card-body"  >
+            <p class="card-text">La flor es la estructura reproductiva característica de las plantas llamadas espermatofitas o fanerógamas. La función de una flor es producir semillas a través de la reproducción sexual.</p>
+            <img src="https://enviocoronas.com/wp-content/uploads/flores-de-primavera-principal.jpg" class="card-img-top" alt="...">
+          </div>
+        </div>
+
+      </div>
+      <div class="col-md-6">
+        <div class="card">
+          <div  style="background-color: #85fa68;" class="card-header">
+            <h6>Arboles</h6>
+
+          </div>
+          <div class="card-body">
+            
+            <p class="card-text">Un árbol es una planta leñosa de más de tres metros de alto que tiene un tallo en la base y forma un tronco o fuste y se ramifica para formar una copa donde están los hojas. Los árboles pueden vivir más que cualquier otro tipo de planta.</p>
+            <img src="https://www.reddearboles.org/nwlib6/includes/phpthumb/phpThumb.php?src=/imagenes/nwproject5.forms.f_news/1/blog-arbol4_11zon.webp&w=700&f=webp" class="card-img-top" alt="150">
+            
+          </div> 
+        </div>
+      </div>
+
+    </div>
+    <!-- CIERRE DEL PRIMER ROW -->
+     <!-- INICIO DEL SEGUNDO ROW -->
+      <div class="row mt-4" >
+        <div class="col-md-4">
+          <div class="card" style="width: 18rem;">
+            <img src="https://www.florclick.com/entradas_blog/calendario-de-flores-5f6bdc06188c5.jpg" class="card-img-top" alt="...">
+            <div class="card-body text-center">
+              <p class="card-text">La margarita (Bellis perennis) es una planta herbácea de la familia de las asteráceas (Asteraceae) muy utilizada a efectos decorativos mezclada con el césped, por sus colores y su resistencia a la siega</p>
+               
+              <button class="btn btn-warning">Mas informacion</button>
+          
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card" style="width: 18rem;">
+            <img src="https://naukas.com/fx/uploads/2017/11/tulipanes1.jpg" class="card-img-top" alt="...">
+            <div class="card-body text-center">
+              <p class="card-text">Tulipa es un género de plantas perennes y bulbosas perteneciente a la familia Liliaceae, en el que se incluyen los populares tulipanes, nombre común con el que se designa a todas las especies, híbridos y cultivares de este género.</p>
+               
+              <button class="btn btn-warning">Mas informacion</button>
+          
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card" style="width: 18rem;">
+            <img src="https://cdn0.bioenciclopedia.com/es/posts/8/2/5/roble_comun_528_orig.jpg" class="card-img-top" alt="...">
+            <div class="card-body text-center">
+              <p class="card-text">El roble es un árbol de la familia de las fagáceas y un tipo de madera muy resistente y densa. </p>
+               
+              <button class="btn btn-warning">Mas informacion</button>
+          
+            </div>
+          </div>
+        </div>
+        
+          </div>
+        
+      <!-- CIERRE DEL SEGUNDO ROW -->
+   </div>
+    
+</template>
+
+<style scoped>
+/* estilos css */
+</style>
+ 
